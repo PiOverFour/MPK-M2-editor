889addd57996a7c68724a477acfd7eece6524a20
Damien Picard <dam.pic@free.fr>
2017-04-17 04:00:17 +0200

    Comment

--- ui/autoFill.py
+++ ui/autoFill.py
@@ -27,7 +27,7 @@ class Ui_autoFill(QtWidgets.QMainWindow):
         [0, 1, 2, 3, 4, 5,  6,  7],  # chromatic
         [0, 2, 4, 5, 7, 9, 11, 12],  # major
         [0, 2, 3, 5, 7, 8, 10, 12],  # minor
-        [0, 2, 4, 5, 7, 8, 11, 12],  # harmonic
+        [0, 2, 4, 5, 7, 8, 11, 12],  # harmonic minor
         [0, 2, 3, 5, 7, 9, 10, 12],  # dorian
         [0, 1, 3, 5, 7, 8, 10, 12],  # phrygian
         [0, 2, 4, 6, 7, 9, 11, 12],  # lydian
