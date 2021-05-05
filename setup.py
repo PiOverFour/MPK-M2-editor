a74687e724f20224c5b07df3d57df9cb4b87fe40
Damien Picard <dam.pic@free.fr>
2021-05-06 00:05:50 +0200

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup
 
 setup(
     name='mpk-m2-editor',
+    description='An alternative to the official AKAI MPKMini MkII Editor',
+    author='Damien Picard',
+    author_email='dam.pic@free.fr',
+    url='https://github.com/PiOverFour/MPK-M2-editor',
     packages=['ui'],
     install_requires=['python-rtmidi', 'pyqt5'],
-    scripts=['mpk-m2-editor.py']
+    scripts=['mpk-m2-editor.py'],
+    include_package_data=True
 )
